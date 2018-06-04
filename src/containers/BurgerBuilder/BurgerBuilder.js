2647499860a8c9ad140d0bc619b0bb7c00689ef2
Barry Hood <barry.hood@connect-group.com>
2018-06-04 17:21:07 +0100

    Dynamic ingredients

---
+++ src/containers/BurgerBuilder/BurgerBuilder.js
@@ -0,0 +1,24 @@
+import React, { Component } from 'react';
+import Auxiliary from '../../hoc/Auxiliary';
+import Burger from '../../components/Burger/Burger';
+
+class BurgerBuilder extends Component {
+  state = {
+    ingredients: {
+      salad: 0,
+      bacon: 0,
+      cheese: 1,
+      meat: 0
+    }
+  }
+  render() {
+    return (
+      <Auxiliary>
+        <Burger ingredients={this.state.ingredients} />
+        <div>Build Controls</div>
+      </Auxiliary>
+    );
+  }
+}
+
+export default BurgerBuilder;
