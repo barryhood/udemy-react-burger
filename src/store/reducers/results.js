b21cd4fdbf231e0896ee9d6ac692dba55b32363c
Barry Hood <barry.hood@connect-group.com>
2018-06-18 09:27:27 +0100

    Leaner action crators/reducers

--- src/store/reducers/results.js
+++ src/store/reducers/results.js
@@ -5,19 +5,19 @@ const initialState = {
   results: []
 }
 
+const deleteResult = (state, action) => {
+  const updatedArray = state.results.filter(result => { // use filter to return array immutably
+    return result.id !== action.resultId
+  });
+  return updateObject(state, {results: updatedArray});
+};
+
 const resultsReducer = (state = initialState, action) => {
   switch (action.type) {
     case actionTypes.STORE_RESULT:
-    return updateObject(state, {
-      results: state.results.concat({ 
-        id: new Date(), 
-        value: action.counter})
-    })
+      return updateObject(state, {results: state.results.concat({id: new Date(), value: action.counter})});
     case actionTypes.DELETE_RESULT:
-      const updatedArray = state.results.filter(result => { // use filter to return array immutably
-        return result.id !== action.resultId
-      });
-      return updateObject(state, {results: updatedArray});
+      return deleteResult(state, action);
     default:
       return state;
   }
